9e31b5ce3e07e54071cbe0dc00b4b8c7bf7fac78
yas-amini <yaasamin.amini@gmail.com>
2025-05-18 16:07:03 +0200

    Added nodemon

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+//const express = require("express");
 import express from "express";
-
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  const html = `
+  <!DOCTYPE>
+  <html>
+  <head>
+  <title>TODO</title>
+    </head>
+    <body>
+    <h1>TODO</h1>
+      <ul>
+        <li>Clean</li>
+        <li>Wash</li>
+        <li>Shopping</li>
+
+      </ul>
+    </body>
+  </html>
+  `;
+  res.send(html);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
+});
