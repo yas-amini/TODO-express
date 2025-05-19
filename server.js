4ac70a702a104598b39753fe779871cfd85a9957
yas-amini <yaasamin.amini@gmail.com>
2025-05-19 15:53:38 +0200

    Installed ejs

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //const express = require("express");
 import express from "express";
-import path from "path";
-
+//import path from "path";
 const app = express();
 const PORT = 3000;
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   //get the content of the file index.html and return to client/browser
   const file = "C:\\Personal Projects\\TODO-express\\public\\index.html";
-  console.log(file);
 
-  res.sendFile("Hello Express!");
+  //res.sendFile(file);
+  res.render("index");
 });
 
 app.listen(3000, () => {
