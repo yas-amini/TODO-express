784a034f3c85fc9018759f2b8a492d8546ff5bd1
yas-amini <yaasamin.amini@gmail.com>
2025-05-19 15:42:50 +0200

    Fixed dirname and moved index and is now an ejs file

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ import express from "express";
 import path from "path";
 
 const app = express();
-
+const PORT = 3000;
 app.get("/", (req, res) => {
   //get the content of the file index.html and return to client/browser
-  const file = path.join(__dirname, "public", "index.html");
+  const file = "C:\\Personal Projects\\TODO-express\\public\\index.html";
   console.log(file);
 
-  res.send("Hello Express!");
+  res.sendFile("Hello Express!");
 });
 
 app.listen(3000, () => {
-  console.log("Listening on http://localhost:3000");
+  console.log(`Listening on http://localhost:${PORT}`);
 });
